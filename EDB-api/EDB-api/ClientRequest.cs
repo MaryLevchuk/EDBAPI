3fcb155a572f5c30243baa427302f2972a5f0465
Mary Levchuk <maria.levchuk@creuna.com>
2018-02-16 09:08:18 +0200

    New step

---
+++ EDB-api/EDB-api/ClientRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using RestSharp;
+
+namespace EDB_api
+{
+    public class ClientRequest
+    {
+        public IRestClient Client;
+
+        public ClientRequest()
+        {
+            Client = new RestClient();
+            Client.BaseUrl = "";
+        }
+    }
+}
